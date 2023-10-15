16bd2dae5c3b7ac67d29063ade3c60f9b5af5f1b
Sebastian Fernandez <seb.flipflip@gmail.com>
2023-10-15 14:14:14 -0600

    run script

--- JARS/run.sh
+++ JARS/run.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <number of times to run mom.jar> <number of times to run client.jar>"
+# Default values
+num_mom_runs=0
+num_client_runs=0
+
+# Parse command line options
+while getopts "m:c:" opt; do
+  case $opt in
+    m)
+      num_mom_runs=$OPTARG
+      ;;
+    c)
+      num_client_runs=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Ensure both options were provided
+if [ "$num_mom_runs" -eq 0 ] || [ "$num_client_runs" -eq 0 ]; then
+  echo "Usage: $0 -m <num_mom_runs> -c <num_client_runs>"
   exit 1
 fi
 
-num_mom_runs=$1
-num_client_runs=$2
-
-echo -n > ports.config
-
+# Run "java -jar mom.jar"
 for ((i=1; i<=num_mom_runs; i++)); do
   echo "Running mom.jar #$i"
   gnome-terminal -- java -jar mom.jar
 done
 
+# Run "java -jar client.jar"
 for ((i=1; i<=num_client_runs; i++)); do
   echo "Running client.jar #$i"
   gnome-terminal -- java -jar client.jar
